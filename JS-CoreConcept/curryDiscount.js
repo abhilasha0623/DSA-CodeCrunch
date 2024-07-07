2fed218b63ce0c3426845f9180c905474280a81b
abhilasha0623 <abhilasha.92.kp@gmail.com>
2024-07-07 20:22:00 +0530

    curryProb

---
+++ JS-CoreConcept/curryDiscount.js
@@ -0,0 +1,9 @@
+const priceCal=(price)=>{
+    return(discount)=>{
+        return price*discount
+    }
+}
+
+//getPrice()
+//let discountAmount=priceCal(300,.3)
+//let discountAmount=priceCal(300,.5)
